a3e0d40f636966e048918e7aff8ea15e7c597d99
June Azuela <violetlovato05@gmail.com>
2021-10-31 10:32:44 +0800

    just saving something?

---
+++ namae.py
@@ -0,0 +1,38 @@
+# I'll try to have a different approach? If I can
+# It should be able to print "Hi, my name is _____. I am ____ years old and I live in _____ ."
+
+def ArtistName():
+    given = input("First Name: " )
+    last = input ("Last Name: ")
+    return given + " " + last 
+
+name = ArtistName()
+
+def ArtistAge():
+    toshi = input("Age: ")
+    return toshi
+
+age = ArtistAge()
+
+def ArtistAdress(Address):
+    print("I live in" + Address )
+
+ArtistAdress(input("Address: "))
+
+print(ArtistAdress)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+

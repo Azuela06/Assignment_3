8d5c6f3f3e568ed5435db2b9c15a00d33b5bd26d
June Azuela <violetlovato05@gmail.com>
2021-10-31 18:24:38 +0800

    AHHHH the apple worked

---
+++ total_amount.py
@@ -0,0 +1,24 @@
+#another item huhu
+# what I need to do this time is to know how much the apples and orange if in a certain value
+# To find the amount of apple I think I should multiply first
+# just like the apple, multiply first 
+# then add. Right?
+
+def TotalApple(applePrice,numberApple):
+    product = applePrice*numberApple
+    return product
+
+Apple = 20
+appleYouWant = int(input("How many apples do you want: "))
+
+TotalApple (Apple, appleYouWant)
+
+def TotalOrange(orangePrice,numberOrange):
+    Product = orangePrice*numberOrange
+    return Product
+
+Orange = 25
+orangeYouWant = int(input("How many Oranges do you want: "))
+
+print(TotalOrange(Orange,orangeYouWant))
+
